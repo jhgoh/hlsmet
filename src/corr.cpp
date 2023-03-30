dc1a9fb844697d0ed1b5578982d5993eee7fd460
ParticleChef <demonichje@gmail.com>
2023-03-29 23:57:41 -0500

    MET is already calculated

--- src/corr.cpp
+++ src/corr.cpp
@@ -10,12 +10,14 @@ HLS implementation of MET calculation from PF objects
 #endif
 
 // pt, phi are integers
-void corr_hw(pt_t data_pt[NPART], phi_t data_phi[NPART], phi_t data_eta[NPART], 
-		pt_t jet_pt[NPART], phi_t jet_phi[NPART], 
-		pt2_t& res_pt2, phi_t& res_phi, pt2_t& corr_pt2){
+void corr_hw(pt_t data_pt[NPART], phi_t data_phi[NPART], 
+		pt_t jet_pt[NPART], phi_t jet_phi[NPART], phi_t jet_eta[NPART], 
+		 pt2_t& corr_pt2){
     #pragma HLS ARRAY_PARTITION variable=data_pt complete
     #pragma HLS ARRAY_PARTITION variable=data_phi complete
-    #pragma HLS ARRAY_PARTITION variable=data_eta complete
+    #pragma HLS ARRAY_PARTITION variable=jet_pt complete
+    #pragma HLS ARRAY_PARTITION variable=jet_phi complete
+    #pragma HLS ARRAY_PARTITION variable=jet_eta complete
 
     #pragma HLS pipeline ii=54
     
@@ -23,24 +25,24 @@ void corr_hw(pt_t data_pt[NPART], phi_t data_phi[NPART], phi_t data_eta[NPART],
 
     pt_t data_pt_int[NPART];
     phi_t data_phi_int[NPART];
-    phi_t data_eta_int[NPART];
     #pragma HLS ARRAY_PARTITION variable=data_pt_int complete
     #pragma HLS ARRAY_PARTITION variable=data_phi_int complete
-    #pragma HLS ARRAY_PARTITION variable=data_eta_int complete
 
     pt_t data_J_pt_int[NPART];
     phi_t data_J_phi_int[NPART];
+    phi_t data_J_eta_int[NPART];
     #pragma HLS ARRAY_PARTITION variable=data_J_pt_int complete
     #pragma HLS ARRAY_PARTITION variable=data_J_phi_int complete
+    #pragma HLS ARRAY_PARTITION variable=data_J_eta_int complete
 
     FILL_INTERNAL: for(int i=0; i<NPART;i++){
 #pragma HLS UNROLL
         data_pt_int[i]=data_pt[i];
         data_phi_int[i]=data_phi[i];
-        data_eta_int[i]=data_eta[i];
 
         data_J_pt_int[i]=jet_pt[i];
         data_J_phi_int[i]=jet_phi[i];
+        data_J_eta_int[i]=jet_eta[i];
     }
 
     // calc signed components first
@@ -59,59 +61,33 @@ void corr_hw(pt_t data_pt[NPART], phi_t data_phi[NPART], phi_t data_eta[NPART],
     }
 
 
-    pxy_t sum_x = 0;
-    pxy_t sum_y = 0;
     pxy_t sumJ_x = 0;
     pxy_t sumJ_y = 0;
     SUM: for(int i=0; i<NPART;i++){
 #pragma HLS UNROLL
-        // Add to x, y sums
-        sum_x -= met_x[i];
-        sum_y -= met_y[i];
-        sumJ_x += jet_x[i];
-        sumJ_y += jet_y[i];
+		if ( data_J_eta_int[i] < int(1.3 * (1<<PHI_SIZE)) || 
+				data_J_eta_int[i] > -int(1.3 * (1<<PHI_SIZE)) ){
+        	sumJ_x += (0.106*jet_x[i]+6.6);
+        	sumJ_y += (0.106*jet_y[i]+6.6);
+		}
          if(!DEBUG){
-             std::cout << "     met x,y = (" << -met_x[i] << ", " << -met_y[i] << ") \t";
-             std::cout << " sum x,y = (" << sum_x << ", " << sum_y << ") \t";
+             std::cout << " sum x,y = (" << sumJ_x << ", " << sumJ_y << ") \t";
              std::cout << " eta  = (" << data_eta[i] << ", ) \t";
              std::cout << " phi  = (" << data_phi[i] << ", ) \t";
              std::cout << " \n";
          }
     }
-	// mean of jet pt	
-	pxy_t mean_x = sumJ_x / NPART;
-	pxy_t mean_y = sumJ_y / NPART;
 
-	// SUM of (x - mean)*(x -mean)
-	pt2_t dev_x = 0;
-	pt2_t dev_y = 0;
-	for(int i=0; i<NPART; i++){
-		dev_x += ( (jet_x[i] - mean_x) * (jet_x[i] - mean_x) );
-		dev_y += ( (jet_y[i] - mean_y) * (jet_y[i] - mean_y) );
-	}
 
-	// stdev of jet
-	pt2_t sigma2_x = dev_x / (NPART-1);
-	pt2_t sigma2_y = dev_y / (NPART-1);
-
-	// sqrt(sigma2)
-	pt_t sigma_x = hls::sqrt(sigma2_x); 
-	pt_t sigma_y = hls::sqrt(sigma2_y);
-
-	// MET * MET
-    res_pt2 = sum_x*sum_x + sum_y*sum_y;
-
-	// sqrt(MET * MET)
-	pt_t res_pt = hls::sqrt(res_pt2);
 
 	// correctd MET
 	// K factor?
-	pt_t K_value = 1.;
+	pt_t K_value = 1.5;
 	pt2_t corr_x;
 	pt2_t corr_y;
 
-	corr_x = sum_x + (K_value * sigma_x);
-	corr_y = sum_y + (K_value * sigma_y);
+	corr_x = met_x + (K_value * sumJ_x);
+	corr_y = met_y + (K_value * sumJ_y);
 
 	corr_pt2 = corr_x*corr_x + corr_y*corr_y; 
     //PhiFromXY(sum_x,sum_y,res_phi);
